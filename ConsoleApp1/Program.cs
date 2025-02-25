722c4bf82050f17e2cbe71df89c90f96c3ebe68f
nastia <mka@A34PC8>
2025-02-25 12:32:37 +0200

    eshkere

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("-------- Welcome to supermarket");
+Console.WriteLine("---Menu---");
+Console.WriteLine("1. add new product");
+Console.WriteLine("2. search product");
+Console.WriteLine("3. delete product");
+Console.WriteLine("4. buy product");
