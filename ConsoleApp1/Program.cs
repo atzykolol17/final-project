7e34b5835352e3aefc8b84579b5de48dfadbac7e
mka <mka@A34PC8>
2025-04-08 14:23:57 +0300

    hkhkj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@
 
 
 // See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
 Console.WriteLine("-------- Welcome to supermarket");
 Console.WriteLine("---Menu---");
 Console.WriteLine("1. add new product");
@@ -21,3 +23,35 @@ Console.WriteLine("3. load to file");
 Console.WriteLine("4. search product by name");
 Console.WriteLine("5. delete product");
 Console.WriteLine("6. sell product");
+
+List<supermarket> supermarketList = new();
+
+while (true)
+{
+    Console.Write("Select menu option: ");
+    int option = int.Parse(Console.ReadLine());
+
+    switch (option)
+
+
+
+    case 1:
+        foreach (var com in product)
+        {
+            Console.WriteLine("--------------add product -------------");
+            Console.WriteLine($"Name: {com.Name}");
+            Console.WriteLine($"Distance: {com.Distance}m");
+        }
+        break;
+
+
+
+    case 2:
+        string supermarketJson = JsonSerializer.Serialize(supermarketList);
+        File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/supermarket.json", supermarketJson);
+        break;
+
+    case 3:
+        string csupermarketJsonRead = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/supermarket.json");
+        supermarketList = JsonSerializer.Deserialize<List<supermarket>>(supermarketJsonRead);
+        break;
